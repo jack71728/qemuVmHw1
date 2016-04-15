4e553e12d9e613487d55f66461a0abd7919cd6f5
JACK <jack71728s251728@yahoo.com.tw>
2016-04-16 01:55:34 +0800

    QQ ssss tb

--- qemu-0.13.0/optimization.c
+++ qemu-0.13.0/optimization.c
@@ -95,12 +95,10 @@ void push_shack(CPUState *env, TCGv_ptr cpu_env, target_ulong next_eip)
 	tcg_temp_free_ptr(temp_shack_top);
 	tcg_temp_free_ptr(temp_shack_end);
 
-	TranslationBlock *tb;
 	tb_page_addr_t phys_pc = get_page_addr_code(env, next_eip);
-	TranslationBlock **ptb1 = &tb_phys_hash[(unsigned int)tb_phys_hash_func(phys_pc)];
+	TranslationBlock *tb = tb_phys_hash[(unsigned int)tb_phys_hash_func(phys_pc)];
 	for (;;)
 	{
-		tb = *ptb1;
 		if (!tb)
 		{
 			list_t *old_list = ((list_t *)env->shadow_hash_list) + tb_jmp_cache_hash_func(next_eip);
@@ -111,12 +109,12 @@ void push_shack(CPUState *env, TCGv_ptr cpu_env, target_ulong next_eip)
 			old_list->next = &new_pair->l;
 			break;
 		}
-		if (tb->pc == next_eip)
+		else if (tb->pc == next_eip)
 		{
 			env->shadow_ret_addr[env->shadow_ret_count] = (unsigned long)tb->tc_ptr;
 			break;
 		}
-		ptb1 = &tb->phys_hash_next;
+		tb = tb->phys_hash_next;
 	}
 
 	env->shadow_ret_count++;
